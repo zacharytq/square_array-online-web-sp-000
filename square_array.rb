a36896e431c20816b3c36410c8516495bcb549e3
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 00:34:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   return_array = []
   array.each do |square|
-    return_array << Integer.sqrt(square)
+    return_array << Math.sqrt(square)
   end
   return_array
 end
