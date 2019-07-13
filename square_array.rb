f6090b567e71d0cf8f4308a1359379da4a669014
chic-page-6502 <zacharytq@gmail.com>
2019-07-13 00:28:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
   # your code here
+  return_array = []
   array.each do |square|
-    square^2
-    puts square
+    return_array << square^2
   end
+  return_array
 end
 
 square_array([2, 4])
